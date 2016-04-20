6247ded0601c729a9501ccf1303085f16ac67a3f
ashishmagar1 <onlylegendery@gmail.com>
2016-04-20 10:34:10 +0545

    this is portfolio page

---
+++ portfolio.php
@@ -0,0 +1,142 @@
+    <section id="portfolio" class="bg-light-gray">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center swing animated">
+                    <h2 class="section-heading">Portfolio</h2>
+                    <h3 class="section-subheading text-muted">My Work</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a href="#portfolioModal1" class="portfolio-link" data-toggle="modal">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/aid2.png" class="img-responsive" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                        <h4>Online Pet Store AID-A 1</h4>
+                        <p class="text-muted">Website Design</p>  
+
+                        <button><h4><a href="http://k9nepal.netne.net/"> <b>Visit this site </b></a></h4> </button>                           
+                            
+                    </div>
+                </div>
+                
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a href="#portfolioModal2" class="portfolio-link" data-toggle="modal">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/k9.png" class="img-responsive" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                        <h4>Online Pet Store AID-A 2</h4>
+                        <p class="text-muted">Website Design</p>
+                        <button><h4><a href="http://ashish1.netai.net/"> <b>Visit this site </b></a></h4> </button>
+
+                    </div>
+                </div>
+             
+               
+
+
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a href="#portfolioModal3" class="portfolio-link" data-toggle="modal">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/ci.png" class="img-responsive" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                        <h4>Online Pet Store AID-A 3 (CI)</h4>
+                        <p class="text-muted">Website Design</p>
+                        <button><h4><a href="http://k9nepal.netai.net/"> <b>Visit this site </b></a></h4> </button>
+                </div>
+                </div>
+
+                 
+
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a href="#portfolioModal5" class="portfolio-link" data-toggle="modal">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/asd.png" class="img-responsive" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                        <h4>Online Automobile Store</h4>
+                        <p class="text-muted">Website Design</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a href="#portfolioModal6" class="portfolio-link" data-toggle="modal">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+
+                        <img src="img/portfolio/2.png" class="img-responsive" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                        <h4>Super Fitness Center Logo</h4>
+                        <p class="text-muted">Graphic/Logo Design</p>
+                    </div>
+                </div>
+
+
+
+
+
+   <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a href="#portfolioModal9" class="portfolio-link" data-toggle="modal">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+
+                        <img src="img/portfolio/ax.png" class="img-responsive" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                        <h4>House possible looks after ACP board installation</h4>
+                        <p class="text-muted">Graphic/Logo Design</p>
+                    </div>
+                </div>
+
+
+
+
+
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a href="#portfolioModal7" class="portfolio-link" data-toggle="modal">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        
+                        <img src="img/portfolio/vc.png" class="img-responsive" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                        <h4>House possible looks after ACP board installation</h4>
+                        <p class="text-muted">Graphic Design</p><br><br>
+                    
+            </div>
+           
+        </div>
+
+<a href="#comment" class="page-scroll btn btn-xl">Make Comment</a>
+
+    </section>
+
