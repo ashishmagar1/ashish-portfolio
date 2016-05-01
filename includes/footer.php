cedcb522f1d029edd89cee7dccaa797067fd9ed1
ashish magar <onlylegendery@gmail.com>
2016-05-01 09:03:42 +0545

    Added files via upload

---
+++ includes/footer.php
@@ -0,0 +1,26 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div >
+                    <span class="copyright"><b>Copyright &copy; 2016-<?php echo date("Y");?>, ID: 77171463, Name: Ashish Magar, Email: onlylegendery@gmail.com, College: The British College, Trade Tower, Thapathali, Kathmandu, Nepal.</b></span>
+                </div>
+                <div >
+                    <ul class="list-inline social-buttons">
+                            <li><a href="https://np.linkedin.com/in/ashish-magar-21554b119"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                            <li><a href="https://github.com/ashishmagar1"><i class="fa fa-github"></i></a>
+                            </li>
+                            <li><a href="https://twitter.com/onlylegendery"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li><a href="https://www.facebook.com/ashish.magar.54"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            
+                            
+                        </ul>
+                        <b>Linkedin | Github | Twitter | Facebook </b>
+                </div>
+                
+            </div>
+        </div>
+        <div id="backtotop" align="right"><a href="#page-top" class="topOfPage"><b><h4>To The Top</h4></b></a></div>
+    </footer>
---
+++ includes/navigation.php
@@ -0,0 +1,67 @@
+  
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                        <img src="img/logos/2.jpg" class="img-responsive img-circle swing animated" alt="">
+                    </a>
+                <a class="navbar-brand page-scroll swing animated" href="#page-top">ASHISH MAGAR <br><br> PORTFOLIO</a><br><br><br><br>
+
+                
+            </div>
+
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right swing animated">
+
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+       
+                    <li>
+                        <a class="page-scroll" href="#page-top">Home</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">Services</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#portfolio">Portfolio</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#blog">Blog</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#cv">CV</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                    <div align="center">
+                    <ul class="list-inline social-buttons">
+                         <li><a href="https://np.linkedin.com/in/ashish-magar-21554b119"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                            <li><a href="https://github.com/ashishmagar1"><i class="fa fa-github"></i></a>
+                            </li>
+                        <li><a href="https://twitter.com/onlylegendery"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li><a href="https://www.facebook.com/ashish.magar.54"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            
+                    </ul>
+
+                    
+                </div>
+                </ul>
+            </div>
+        </div>
+    </nav>
