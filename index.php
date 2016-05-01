8fe3bc57c505e5450567a05728c71c7e701e32c8
ashish magar <onlylegendery@gmail.com>
2016-05-01 10:10:07 +0545

    Added files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>ashish magar portfolio</title>
+    
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/agency.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body id="page-top" class="index">
+
+<?php
+include("includes/navigation.php");
+?>
+
+<?php
+include("files/home.php");
+?>
+
+<?php
+include("files/services.php");
+?>
+
+<?php
+include("files/portfolio.php");
+?>
+
+<?php
+include("files/blog.php");
+?>
+
+
+
+<?php
+include("files/about.php");
+?>
+
+<?php
+include("files/cv.php");
+?>
+
+<?php
+include("files/contact.php");
+?>
+
+<?php
+include("files/portfoliodetail.php");
+?>
+
+<?php
+include("includes/footer.php");
+?>
+    
+
+    
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+    <script src="js/agency.js"></script>
+
+</body>
+</html>
